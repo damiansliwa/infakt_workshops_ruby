b3733e7f600b831df73a630d8d861d19c7a026c9
Damian Śliwa <damian.sliwa.90@gmail.com>
2023-04-16 12:13:16 +0200

    Add regex to validation password length, presence small and capital letters, and digits

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,15 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
+  def perform
 
+    regex = /^(?=.{8,24}$)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*/
+
+    if @password =~ regex
+      return true
+    else
+      return false
+    end
+  end
 end
